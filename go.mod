8fa7f3584450c3508e4275c9ba87555c50a6b74e
Tom Berger <tom@getplayback.com>
2022-08-29 23:32:11 -0400

    Add db mappings for network names

--- go.mod
+++ go.mod
@@ -2,10 +2,21 @@ module github.com/playback-sports/sportradar
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.0
+require (
+	github.com/playback-sports/db v0.0.0-20220830033003-37850f3ca074
+	github.com/rs/zerolog v1.28.0
+	github.com/stretchr/testify v1.8.0
+	github.com/urfave/cli/v2 v2.11.2
+)
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/client_test.go
+++ pkg/client_test.go
@@ -22,7 +22,7 @@ func TestMLBDailySummary(t *testing.T) {
 	summary, err := client.MLBDailySummary(now)
 	assert.NoError(t, err)
 	assert.Equal(t, "MLB", summary.League.Alias)
-	assert.True(t, len(summary.League.Games) > 1)
+	assert.NotEmpty(t, summary.League.Games)
 	assert.NotEmpty(t, summary.League.Games[0].ID)
 }
 
@@ -37,7 +37,7 @@ func TestMLBSchedule(t *testing.T) {
 	schedule, err := client.MLBSchedule(now, sr.SeasonTypeRegular)
 	assert.NoError(t, err)
 	assert.Equal(t, "MLB", schedule.League.Alias)
-	assert.True(t, len(schedule.Games) > 1)
+	assert.NotEmpty(t, schedule.Games)
 	assert.NotEmpty(t, schedule.Games[0].ID)
 	assert.NotEmpty(t, schedule.Games[0].Broadcast)
 	assert.NotEmpty(t, schedule.Games[0].Broadcast.Network)
@@ -54,7 +54,7 @@ func TestNFLSchedule(t *testing.T) {
 	schedule, err := client.NFLSchedule(now, sr.SeasonTypeRegular)
 	assert.NoErrorf(t, err, "%s", err)
 	assert.NotEmpty(t, schedule.ID)
-	assert.True(t, len(schedule.Weeks) > 1)
+	assert.NotEmpty(t, schedule.Weeks)
 	assert.NotEmpty(t, schedule.Weeks[0].ID)
 	assert.NotEmpty(t, schedule.Weeks[0].Games)
 	assert.NotEmpty(t, schedule.Weeks[0].Games[0].ID)
